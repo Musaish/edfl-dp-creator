2f10e3cae9567793e24b946f6886a4e6f22a97bb
Ibrahim Salami <ibrahimsalami@Ibrahim-Salamis-Mac.local>
2025-04-05 15:36:19 +0100

    my

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ const App = () => {
                 <img
                   src={generatedImage}  
                   alt="Generated DP"
-                  className="w-full h-full lg:w-[1080px] lg:h-[1080px] "
+                  className="w-full h-full lg:w-[1080px] lg:h-[1080px] object-cove "
                 />
               )}
               <div className=' p-6'>
